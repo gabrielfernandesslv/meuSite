02fc63e796c506888ac5ae52d805dbf35538e4d7
Gabriel <gaasilva0912@gmail.com>
2019-10-10 08:13:34 -0300

    upando as pgs

--- meuSite.py
+++ meuSite.py
@@ -6,9 +6,17 @@ app = Flask (__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     return render_template("login.html")
+
+@app.route('/objetivo')
+def objetivo():
+    return render_template("objetivo.html")
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
