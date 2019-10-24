03dc68002449369b252d70fda483687284bbf950
Gabriel <gaasilva0912@gmail.com>
2019-10-24 08:36:05 -0300

    add o db

--- meuSite.py
+++ meuSite.py
@@ -1,6 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meuSite.db'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/')
 def index():
