73c065f30a9026f70a72b3b1463a323681cc5db9
Gabriel <gaasilva0912@gmail.com>
2019-12-05 08:56:54 -0300

    correcaofinal

--- forms.py
+++ forms.py
@@ -4,12 +4,21 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class registratioform(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired(),Length(min=2,max=80)])
+
     idade = IntegerField ('Idade', validators=[DataRequired()])
+
     cartaosus = IntegerField ('Cartão de SUS', validators=[DataRequired()])
+
     endereco = StringField ('Endereço', validators=[DataRequired()])
+
     telefone = IntegerField ('Telefone', validators=[DataRequired()])
+
     cidade_estado = StringField ('Cidade-Estado', validators=[DataRequired()])
+
     email = StringField('Email', validators=[DataRequired(),Email()])
-    password = PasswordField('Senha', validators=[DataRequired(),EqualTo('confirm_password')])
-    confirm_password = PasswordField('Repete Senha', validators=[DataRequired()])
+
+    password = PasswordField('Senha', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Repete Senha', validators=[DataRequired(), EqualTo('password')])
+    
     botao = SubmitField('Enviar')
--- meuSite.py
+++ meuSite.py
@@ -10,18 +10,20 @@ db = SQLAlchemy(app)
 class Usuario(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), unique=True, nullable=False)
+    nome = db.Column(db.String(50), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
     cartaosus = db.Column(db.Integer, unique=True, nullable=False)
-    endereco = db.Column(db.String(200), unique=True, nullable=False)
-    telefone = db.Column(db.String(14), unique=True, nullable=False)
+    endereco = db.Column(db.String(200), nullable=False)
+    telefone = db.Column(db.String(14), nullable=False)
     cidade_estado = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(8), nullable=False)
 
+
     def __repr__(self):
         return "<User %r>" % self.username
 
+db.create_all()
 
 @app.route('/')
 def index():
@@ -37,16 +39,22 @@ def objetivo():
 
 @app.route('/form', methods=['POST', 'GET'])
 def form():
-
     form = registratioform()
-
-    if form.validate_on_submit:
-        novo_user = Usuario(nome=form.nome.data, idade=form.idade.data, 
-        cartaosus=form.cartaosus.data, endereco=form.endereco.data, telefone=form.telefone.data, cidade_estado=form.cidade_estado.data, 
-        email=form.email.data, password=form.password.data)
+    
+    if form.validate_on_submit():
+        novo_user = Usuario()
+        novo_user.nome=form.nome.data
+        novo_user.idade=form.idade.data 
+        novo_user.cartaosus=form.cartaosus.data
+        novo_user.endereco=form.endereco.data
+        novo_user.telefone=form.telefone.data
+        novo_user.cidade_estado=form.cidade_estado.data
+        novo_user.email=form.email.data
+        novo_user.password=form.password.data
         db.session.add(novo_user)
-        db.session.commit
-    return render_template("form.html", form=form)
+        db.session.commit()
+
+    return render_template('form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
