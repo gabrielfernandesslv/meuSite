a5a6bd5b4d9a6a0f00e2ab16d7058c3277d0e385
Gabriel <gaasilva0912@gmail.com>
2019-12-04 23:41:44 -0300

    upando as ultimas

--- forms.py
+++ forms.py
@@ -1,17 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import(
-stringfield,
-passwordfield,
-submitfield,
-booleanfield
-)
-from wtforms.validators import datarequired, lenght, email,equalto  
+from wtforms import StringField, SubmitField, PasswordField, DateField, SelectField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo, Length 
 
 class registratioform(FlaskForm):
-    username = stringfield(
-        'usuario',
-        validators=[datarequired(),lenght(min=2,max=80)]
-    )
-    email = stringfield('email', validators=[datarequired(),email()])
-    password = passwordfield('senha', validators=[datarequired()])
-    confirm_password = passwordfield('repete senha', validators=[datarequired(),equalto('password')])
+    nome = StringField('Nome', validators=[DataRequired(),Length(min=2,max=80)])
+    idade = IntegerField ('Idade', validators=[DataRequired()])
+    cartaosus = IntegerField ('Cartão de SUS', validators=[DataRequired()])
+    endereco = StringField ('Endereço', validators=[DataRequired()])
+    telefone = IntegerField ('Telefone', validators=[DataRequired()])
+    cidade_estado = StringField ('Cidade-Estado', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Senha', validators=[DataRequired(),EqualTo('confirm_password')])
+    confirm_password = PasswordField('Repete Senha', validators=[DataRequired()])
+    botao = SubmitField('Enviar')
--- meuSite.py
+++ meuSite.py
@@ -1,30 +1,26 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from forms import registratioform
 
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meuSite.db'
+app.config['SECRET_KEY'] = 'akjdshsagdgajdgmnxmch'
 db = SQLAlchemy(app)
 
 class Usuario(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(8), nullable=False)
+    nome = db.Column(db.String(50), unique=True, nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
+    cartaosus = db.Column(db.Integer, unique=True, nullable=False)
+    endereco = db.Column(db.String(200), unique=True, nullable=False)
+    telefone = db.Column(db.String(14), unique=True, nullable=False)
+    cidade_estado = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
 
     def __repr__(self):
         return "<User %r>" % self.username
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 @app.route('/')
@@ -41,7 +37,16 @@ def objetivo():
 
 @app.route('/form', methods=['POST', 'GET'])
 def form():
-    return render_template("form.html")
+
+    form = registratioform()
+
+    if form.validate_on_submit:
+        novo_user = Usuario(nome=form.nome.data, idade=form.idade.data, 
+        cartaosus=form.cartaosus.data, endereco=form.endereco.data, telefone=form.telefone.data, cidade_estado=form.cidade_estado.data, 
+        email=form.email.data, password=form.password.data)
+        db.session.add(novo_user)
+        db.session.commit
+    return render_template("form.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
